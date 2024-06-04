5059e23ecb1f7df624175ddc188f0e6305d7d3e9
Prabeton <3846205@gmail.com>
2024-06-04 19:45:41 +0200

    nowy deploy na vercelu

--- features/landing/components/Currently.tsx
+++ features/landing/components/Currently.tsx
@@ -1,10 +1,23 @@
 "use client";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 import Link from "next/link";
 
 const Currently = () => {
   return (
     <div className="box-border relative flex items-center justify-center w-screen h-full text-3xl text-yellow bg-dark-gray">
       CURRENTLY
+      <div className="box-border  bg-gray-900 flex items-center justify-center bg-center bg-cover shadow-xl md:w-120 lg:w-120 xl:w-160.75 xl:h-94">
+        <div className="flex flex-col w-full h-9/10 justify-stretch">
+          <LiteYouTubeEmbed
+            id="J0EQpJNgDS8"
+            title="Tytuł filmu z Devstock"
+            poster="hqdefault"
+            playerClass="lty-playbtn"
+            wrapperClass="yt-lite"
+          />
+        </div>
+      </div>
     </div>
   );
 };
